18cf23b06e385eefe9c9e0677025fa1fd450a3dc
WilliamGR-dev <wgirardreydet@gmail.com>
2021-06-27 17:01:42 +0200

    PWA

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import store from './store'
+import './registerServiceWorker'
 
 import { IonicVue } from '@ionic/vue';
 
@@ -23,6 +24,8 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+import './registerServiceWorker'
 
 const app = createApp(App).use(store).use(IonicVue).use(router);
 
